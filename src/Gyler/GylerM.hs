c10d0997b17dff8fed03e50b6736aeca7c123025
Leonid Znamenok <respublica@altlinux.org>
2025-07-08 16:25:23 +0300

    Gyler.GylerM: Define GylerM as ReaderT GylerContext IO.
    This module serves as the main execution context for the Gyler application.
    It provides GylerContext to all functions that require access to external
    resources (e.g., Gyler or Gitery).

---
+++ src/Gyler/GylerM.hs
@@ -0,0 +1,14 @@
+module Gyler.GylerM (
+    GylerM,
+    runGylerM,
+) where
+
+import Control.Monad.Reader (ReaderT, runReaderT)
+import Data.Text (Text)
+
+import Gyler.Context.Gyler (GylerContext)
+
+type GylerM = ReaderT GylerContext IO
+
+runGylerM :: GylerContext -> GylerM a -> IO a
+runGylerM = flip runReaderT
