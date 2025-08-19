a2bf005c61a63243655ef245676ab2039fb306bc
Leonid Znamenok <respublica@altlinux.org>
2025-08-19 22:15:09 +0300

    Gyler.Data.ValidContainer: Add typeclass abstraction
    ValidContainer represents a data structure that can be created
    from a list and provides a function to check whether an element
    is stored.
    
    This typeclass is useful when choosing a data structure based on
    time/space complexity, while preserving a common abstraction across
    different implementations.

---
+++ src/Gyler/Data/ValidContainer.hs
@@ -0,0 +1,16 @@
+module Gyler.Data.ValidContainer (
+    ValidContainer(..),
+) where
+
+-- |
+-- Module: Gyler.Data.ValidContainer
+-- Descripton: Abstract interface for different containers
+--
+-- ValidContainer allows to use different containers with RuntimeValidated
+-- typeclass depending on time/space complexity of data structure.
+
+import Data.Hashable (Hashable)
+
+class ValidContainer c where
+  vcMember :: (Eq a, Hashable a) => a -> c a -> Bool
+  vcFromList :: (Eq a, Hashable a) => [a] -> c a
