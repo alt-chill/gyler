c9f6871f66a021735c28a5c194d3b64cdb6e64c8
Leonid Znamenok <respublica@altlinux.org>
2025-08-28 13:03:42 +0300

    Gyler.Domain: Add Maintainer as a RuntimeValidated type

---
+++ src/Gyler/Domain/Maintainer.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Gyler.Domain.Maintainer (
+    Maintainer,
+    MaintainersSet(..)
+) where
+
+-- |
+-- Module: Gyler.Domain.Maintainer
+-- Description: A RuntimeValidated datatype containing maintainers recognized by girar.
+--
+-- Use RuntimeValidated `mkValidate` to create a new value.
+-- Use `fetch` with `MaintainersQuery` to obtain a `MaintainersSet`.
+--
+-- See: `Gyler.FetchSpec.MaintainersQuery`
+
+import Gyler.Data.NonEmptyText (NonEmptyText)
+
+import Gyler.Classes.RuntimeValidated.Internal (RuntimeValidated(..))
+
+import Data.Hashable (Hashable)
+import Gyler.Data.ValidContainer.HashSet (HashSet)
+
+import GHC.IsList (IsList)
+import Data.Serialize (Serialize)
+
+newtype Maintainer = Maintainer NonEmptyText
+                     deriving Show
+                     deriving newtype (Eq, Hashable, Serialize)
+
+instance RuntimeValidated Maintainer where
+    type Raw Maintainer = NonEmptyText
+    mkUnsafe = Maintainer
+    getRaw (Maintainer x) = x
+
+newtype MaintainersSet = MaintainersSet (HashSet Maintainer)
+                         deriving Show
+                         deriving newtype (Eq, Serialize)
