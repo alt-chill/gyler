ffc14bd3e90d7cb33261ca1847065b95efea60fb
Leonid Znamenok <respublica@altlinux.org>
2025-09-28 00:32:04 +0300

    Gyler.Utils.Maybe: Add `maybeToRight` combinator

--- src/Gyler/Utils/Maybe.hs
+++ src/Gyler/Utils/Maybe.hs
@@ -1,7 +1,12 @@
 module Gyler.Utils.Maybe (
-    rightToMaybe
+    rightToMaybe,
+    maybeToRight
 ) where
 
 rightToMaybe :: Either a b -> Maybe b
 rightToMaybe (Left _)  = Nothing
 rightToMaybe (Right x) = Just x
+
+maybeToRight :: b -> Maybe a -> Either b a
+maybeToRight _ (Just x) = Right x
+maybeToRight y Nothing  = Left y
