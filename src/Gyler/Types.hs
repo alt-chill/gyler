f8a74e0c64390bde5b0056ba9e8ebea158e25064
Leonid Znamenok <respublica@altlinux.org>
2025-09-28 00:25:02 +0300

    Gyler.Types: add function `showCmd :: Cmd -> Text`
    This function is preferable to using `(T.pack . show)` because `Cmd` is
    already stored as `Text` (`NonEmptyText`). This avoids unnecessary
    conversions between `String` and `Text`.

--- src/Gyler/Types.hs
+++ src/Gyler/Types.hs
@@ -1,9 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Gyler.Types (
-    Exec, Args, Cmd
+    Exec, Args, Cmd,
+    showCmd
 ) where
 
-import Gyler.Data.NonEmptyText (NonEmptyText)
+import Data.Text (Text, intercalate)
+
+import Gyler.Data.NonEmptyText (NonEmptyText, toText)
+
 
 type Exec = NonEmptyText
 type Args = [NonEmptyText]
 type Cmd  = (Exec, Args)
+
+showCmd :: Cmd -> Text
+showCmd (a, b) = mconcat ["(", toText a, ", [", (intercalate ", " . fmap toText) b, "])"]
