fe0378e1149b740684f6124c7303e89f83553d0f
Leonid Znamenok <respublica@altlinux.org>
2025-09-30 21:52:59 +0300

    Gyler.Domain: Add Branch as a RuntimeValidated type

---
+++ src/Gyler/Domain/Branch.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Gyler.Domain.Branch (
+    Branch,
+    BranchesSet
+) where
+
+import Gyler.Data.NonEmptyText (NonEmptyText)
+
+import Gyler.Classes.RuntimeValidated.Internal (RuntimeValidated(..))
+
+import Data.Hashable (Hashable)
+import Gyler.Data.ValidContainer.HashSet (HashSet)
+
+import Data.Serialize (Serialize)
+
+newtype Branch = Branch NonEmptyText
+                     deriving Show
+                     deriving newtype (Eq, Hashable, Serialize)
+
+instance RuntimeValidated Branch where
+    type Raw Branch = NonEmptyText
+    mkUnsafe = Branch
+    getRaw (Branch x) = x
+
+type BranchesSet = HashSet Branch
