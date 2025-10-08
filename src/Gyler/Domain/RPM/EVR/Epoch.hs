a6c11691ddae2d8cb4fc2f23385c934d3a3f1d89
Leonid Znamenok <respublica@altlinux.org>
2025-10-08 17:47:21 +0300

    Gyler.Domain.RPM.EVR: Introduce Epoch type
    Epoch parser requires string in the format like "1:".
    
    Make Epoch instance of Serialize and Hashable, so it will be possible to
    use it both with unordered-containers and Gyler.FetchSpec.

---
+++ src/Gyler/Domain/RPM/EVR/Epoch.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+module Gyler.Domain.RPM.EVR.Epoch (
+    -- Data type.
+    Epoch(..),
+
+    -- Parser and smart constructor.
+    epoch,
+    mkEpoch
+) where
+
+-- | Module: Gyler.Domain.RPM.EVR.Epoch
+--
+-- Description: Optional numerical value which can be used to override normal
+-- version-release sorting order.
+--
+-- Non-existent epoch is exactly equal to zero epoch in all version comparisons.
+
+import Numeric.Natural (Natural)
+
+import Gyler.Parsers (Parser, useParser, nonEmptyText)
+
+import Text.Megaparsec (some, eof)
+import Text.Megaparsec.Char (char)
+import Text.Megaparsec.Char.Lexer (decimal)
+
+import Data.Text (Text)
+import qualified Data.Text as T(pack)
+
+import GHC.Generics (Generic)
+import Data.Hashable (Hashable)
+import Data.Serialize (Serialize)
+
+import Gyler.Classes.IsText (IsText(..))
+
+
+newtype Epoch = Epoch Natural
+                    deriving (Eq, Show, Ord, Hashable, Generic)
+
+instance Serialize Epoch
+
+epoch :: Parser Epoch
+epoch = Epoch <$> (decimal <* char ':')
+
+mkEpoch :: IsText e => e -> Maybe Epoch
+mkEpoch txt = case useParser (epoch <* eof) (toText txt) of
+                Right e -> Just e
+                Left  _ -> Nothing
