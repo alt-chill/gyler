5d40a9ace4695fbb1ce48d99e08dd0de884861f9
Leonid Znamenok <respublica@altlinux.org>
2025-10-17 21:04:44 +0300

    Gyler.Domain.RPM: Introduce EVR type

---
+++ src/Gyler/Domain/RPM/EVR.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module Gyler.Domain.RPM.EVR (
+    EVR(..),
+    evrP,
+    mkEvr
+) where
+
+import Gyler.Domain.RPM.EVR.Epoch   (Epoch(..), epochP)
+import Gyler.Domain.RPM.EVR.Version (Version, versionP)
+import Gyler.Domain.RPM.EVR.Release (Release, releaseP)
+
+import Gyler.Domain.RPM.VerCmp (rpmvercmp)
+
+import Gyler.Classes.IsText (IsText (..))
+
+import Gyler.Parsers (Parser, useParser)
+
+import GHC.Generics (Generic)
+import Data.Hashable (Hashable)
+import Data.Serialize (Serialize)
+
+import Data.Maybe (fromMaybe)
+
+import Text.Megaparsec (optional, try, eof)
+import Text.Megaparsec.Char (char)
+
+import Data.Text (Text)
+
+-- Module: Gyler.Domain.RPM.EVR
+--
+-- Description: EVR is used to refer to software versions in RPM,
+-- consisting of up to three components:
+-- * VERSION reflects the actual packaged software version.
+-- * RELEASE reflects packaging revisions within that software version.
+-- * EPOCH is an artificial override to allow working around versioning
+--   anomalies.
+
+data EVR = EVR {
+    _epoch   :: !Epoch,
+    _version :: !Version,
+    _release :: !Release
+} deriving (Show, Eq, Generic)
+
+instance Serialize EVR
+instance Hashable  EVR
+
+instance Ord EVR where
+    compare (EVR e1 v1 r1) (EVR e2 v2 r2)  = compare e1 e2
+                                          <> compare v1 v2
+                                          <> compare r1 r2
+
+{-# ANN module ("HLint: ignore Use <$>" :: String) #-}
+evrP :: Parser EVR
+evrP = do
+    e <- fromMaybe (Epoch 0) <$> (optional . try) epochP
+    v <- versionP
+    _ <- char '-'
+    r <- releaseP
+
+    return (EVR e v r)
+
+mkEvr :: IsText e => e -> Maybe EVR
+mkEvr txt = case useParser (evrP <* eof) (toText txt) of
+            Right evr -> Just evr
+            Left  _   -> Nothing
--- test/Gyler/Domain/RPM/EVRSpec.hs
+++ test/Gyler/Domain/RPM/EVRSpec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Gyler.Domain.RPM.EVRSpec (
     spec
 ) where
@@ -8,8 +10,69 @@ import qualified Gyler.Domain.RPM.EVR.ReleaseSpec as Release
 
 import Test.Hspec
 
+import Test.QuickCheck
+import Data.Maybe (isJust, isNothing, fromMaybe)
+
+import qualified Data.Text as T
+
+import Gyler.Domain.RPM.EVR
+import Gyler.Domain.RPM.EVR.Epoch   (Epoch(..))
+import Gyler.Domain.RPM.EVR.Version (mkVersion)
+import Gyler.Domain.RPM.EVR.Release (mkRelease)
+import Gyler.Classes.IsText (toText)
+
+
 spec :: Spec
-spec = describe "EVR" $ do
+spec = parallel $ describe "EVR" $ do
     Epoch.spec
     Version.spec
     Release.spec
+
+    evrSpec
+
+evrSpec :: Spec
+evrSpec = parallel $ do
+    describe "mkEvr" $ do
+        it "parses full EVR with explicit epoch" $ do
+          let evr = mkEvr ("1:1.2.3-4" :: T.Text)
+          evr `shouldSatisfy` isJust
+
+        it "parses EVR without epoch (defaults to 0)" $ do
+          let evr = mkEvr ("1.2.3-4" :: T.Text)
+          evr `shouldBe` Just (EVR (Epoch 0)
+                                    (fromMaybe (error "bad version") $ mkVersion ("1.2.3" :: T.Text))
+                                    (fromMaybe (error "bad release") $ mkRelease ("4" :: T.Text))
+                                    )
+
+        it "fails on invalid EVR (missing release)" $ do
+          mkEvr ("1.2.3" :: T.Text) `shouldBe` Nothing
+
+        it "fails on invalid characters" $ do
+          mkEvr ("1:1.2.3 4" :: T.Text) `shouldBe` Nothing
+
+        it "fails on empty string" $ do
+          mkEvr ("" :: T.Text) `shouldBe` Nothing
+
+    describe "property-based tests" $ do
+        it "parses valid EVRs successfully" $
+          property $ forAll genValidEvrText $ \txt ->
+            isJust (mkEvr txt)
+
+        it "round-trips" $
+          property $ forAll genValidEvrText $ \txt ->
+            case mkEvr txt of
+              Just evr -> mkEvr (showEVR evr) == Just evr
+              Nothing  -> False
+
+
+
+-- | Generate valid EVR strings such as "1:1.2.3-4", "0:0.1-rc1", "1.0.0-1"
+genValidEvrText :: Gen T.Text
+genValidEvrText = do
+  e <- frequency [(3, pure ""), (1, fmap (\n -> show n <> ":") (choose (0,10000) :: Gen Int))]
+  v <- listOf1 $ elements (['A'..'Z'] ++ ['a'..'z'] ++ ['0'..'9'] ++ "._+~")
+  r <- listOf1 $ elements (['A'..'Z'] ++ ['a'..'z'] ++ ['0'..'9'] ++ "._+~")
+  pure . T.pack $ e <> v <> "-" <> r
+
+showEVR :: EVR -> T.Text
+showEVR (EVR (Epoch e) v r) = (T.pack . show) e <> ":" <> toText v <> "-" <> toText r
