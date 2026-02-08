8bf715e02fb139626c3a016fb359f1d858497c9d
Leonid Znamenok <respublica@altlinux.org>
2026-02-09 01:23:00 +0400

    Gyler.Domain.RuntimeValidated: Make template types IDSerializable
    They now fit to the Gyler domain’s general serialization model
    and no longer differ from other domain types.
    
    This change is required to add the DeriveGeneric extension to modules
    with type definitions and to regenerate the golden output for FetchSpecs
    using genOutput from TestUtils.FetchSpec:
    
    > genOutput MaintainersQuery "test/golden/FetchSpec/MaintainersQuery/input.txt"
    > ...
    
    This is necessary due to a change in the serialized representation of
    fetched data.

--- src/Gyler/Domain/Branch.hs
+++ src/Gyler/Domain/Branch.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeFamilies #-}
--- src/Gyler/Domain/RuntimeValidated/Template.hs
+++ src/Gyler/Domain/RuntimeValidated/Template.hs
@@ -42,36 +42,40 @@ import Text.Megaparsec (some, (<|>), oneOf)
 import qualified Data.Text as T (pack)
 
 import Data.Hashable (Hashable)
-import Data.Serialize (Serialize)
+import GHC.Generics  (Generic)
+
+import Gyler.Serialize (deriveIDSerializable)
 
 mkRvNonEmptyText :: String -> String -> Q [Dec]
-mkRvNonEmptyText typeNameStr setNameStr =
-  let tyName  = mkName typeNameStr
-      setName = mkName setNameStr
+mkRvNonEmptyText typeNameStr setNameStr = do
+        let tyName  = mkName typeNameStr
+            setName = mkName setNameStr
 
-      decs = [d|
-        newtype RV_TYPE = RV_TYPE NonEmptyText
-            deriving stock Show
-            deriving newtype (Eq, Hashable, Serialize)
+        base <- renameByBase "RV_TYPE" tyName . renameByBase "RV_SET" setName <$> [d|
+            newtype RV_TYPE = RV_TYPE NonEmptyText
+                deriving stock (Show, Generic)
+                deriving newtype (Eq, Hashable)
 
-        instance RuntimeValidated RV_TYPE where
-            type Raw RV_TYPE = NonEmptyText
-            mkUnsafe = RV_TYPE
-            getRaw (RV_TYPE x) = x
+            instance RuntimeValidated RV_TYPE where
+                type Raw RV_TYPE = NonEmptyText
+                mkUnsafe = RV_TYPE
+                getRaw (RV_TYPE x) = x
 
-        type RV_SET = HashSet RV_TYPE
+            type RV_SET = HashSet RV_TYPE
 
-        instance HasParsableRaw RV_TYPE where
-            rawParser _ = nonEmptyText (T.pack <$> some (latinAlphaNum <|> oneOf rpmChars))
+            instance HasParsableRaw RV_TYPE where
+                rawParser _ = nonEmptyText (T.pack <$> some (latinAlphaNum <|> oneOf rpmChars))
 
-        instance IsText RV_TYPE where
-            toText (RV_TYPE x) = toText x
+            instance IsText RV_TYPE where
+                toText (RV_TYPE x) = toText x
 
-        instance IsNonEmptyText RV_TYPE where
-            toNonEmptyText (RV_TYPE x) = x
-        |]
+            instance IsNonEmptyText RV_TYPE where
+                toNonEmptyText (RV_TYPE x) = x
+            |]
 
-  in renameByBase "RV_TYPE" tyName . renameByBase "RV_SET" setName <$> decs
+        serialize <- deriveIDSerializable (conT tyName)
+
+        return (base ++ serialize)
 
 -- | Rename every occurrence of a TH Name whose 'nameBase' matches the given string.
 -- Uses SYB (everywhere + mkT) to traverse the whole AST.
